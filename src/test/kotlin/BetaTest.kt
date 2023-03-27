4f72f25401fb70f47c7a4754813f04d4b315c83a
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-27 12:19:06 -0300

    log example

--- src/test/kotlin/BetaTest.kt
+++ src/test/kotlin/BetaTest.kt
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,34 +11,42 @@ $ Project: learning-kotlin
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class BetaTest {
-    var number = 0
+    private val LOG: Logger = LoggerFactory.getLogger(BetaTest::class.java)
+
+    private var number = 0
 
     init {
+        LOG.info("Init")
         number = 10
     }
 
     @BeforeAll
     fun method3() {
+        LOG.info("BeforeAll")
         number += 11
     }
 
     @AfterAll
     fun method4() {
+        LOG.info("AfterAll")
         number /= 3
     }
 
     @BeforeEach
     fun method2() {
+        LOG.info("BeforeEach")
         number -= 4
     }
 
     @AfterEach
     fun method5() {
-        println(number)
+        LOG.info("AfterEach")
+        LOG.info(number.toString())
     }
 
     @Test
     fun method6() {
+        LOG.info("Test1")
         number += 9
     }
 }
