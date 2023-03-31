67213761c511e95ef3f018095adccca25c0564b6
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-31 19:52:04 -0300

    delegate examples

---
+++ src/main/kotlin/delegate6.kt
@@ -0,0 +1,43 @@
+/**
+ * Created with IntelliJ IDEA.
+$ Project: learning-kotlin
+ * User: rodrigotroy
+ * Date: 31-03-23
+ * Time: 13:20
+ */
+interface ICounter {
+    var count: Int
+    fun increaseCount()
+    fun getCurrentCount(): Int
+}
+
+class Counter(override var count: Int) : ICounter {
+    // In case we need to count differently, this function can be altered
+    override fun increaseCount() {
+        count++
+    }
+
+    override fun getCurrentCount(): Int = count
+}
+
+// Do not change the code above
+
+class Iterator(base: ICounter) : ICounter by base {
+    fun next() {
+        increaseCount()
+    }
+}
+
+// Do not change the code below
+
+fun main() {
+    val a = readln().toInt()
+    val counter = Counter(a)
+    val it = Iterator(counter)
+
+    for (i in 1..5) {
+        it.next()
+    }
+
+    println(it.getCurrentCount())
+}
