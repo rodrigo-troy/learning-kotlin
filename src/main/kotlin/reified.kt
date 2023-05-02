50bfc2795e104b6a79ce8bdc06b82a9e4bcc9495
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-05-02 17:43:46 -0400

    reified example

---
+++ src/main/kotlin/reified.kt
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+$ Project: learning-kotlin
+ * User: rodrigotroy
+ * Date: 02-05-23
+ * Time: 17:25
+ */
+inline fun <reified T> filterByType(list: List<Any>): List<T> {
+    val newList = mutableListOf<T>()
+    for (item in list) {
+        if (item is T) {
+            newList.add(item)
+        }
+    }
+    return newList
+}
+
+inline fun <reified T> filterByType2(list: List<Any>): List<T> {
+    return list.filterIsInstance<T>()
+}
+
+fun main() {
+    val mixedList = listOf("Kotlin", 1, 2, 3, "Java", "Android", 4, 5, 6)
+    val stringList1: List<String> = filterByType(mixedList)
+    println(stringList1)
+
+    val stringList2: List<String> = filterByType2(mixedList)
+    println(stringList2)
+}
