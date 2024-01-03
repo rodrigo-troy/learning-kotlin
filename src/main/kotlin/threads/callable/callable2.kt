2d05113bd8e80244b6dd146f4afefb383e6d6599
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2024-01-03 18:49:26 -0300

    Add new file callable2.kt with main method
    Added a new kotlin file, 'callable2.kt', in 'threads/callable' package. This file contains a main method that executes and prints various information along with the thread id. The main method also initiates a Callable object performing certain actions.

---
+++ src/main/kotlin/threads/callable/callable2.kt
@@ -0,0 +1,30 @@
+package threads.callable
+
+import java.time.LocalDateTime
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+/**
+ * Main method to start the program execution.
+ *
+ * This method prints the current date and time along with the thread ID and starts counting.
+ * It creates a Callable object named generator that sleeps for 5 seconds and then prints the current date and time,
+ * thread ID, and returns the value 70000. Finally, it prints the result returned by the generator.
+ */
+fun main() {
+    println("${LocalDateTime.now()} - Thread ID: ${Thread.currentThread().id} - Start counting")
+
+    val executor: ExecutorService = Executors.newSingleThreadExecutor()
+
+    val future = executor.submit(
+        Callable {
+            TimeUnit.SECONDS.sleep(3)
+            println("${LocalDateTime.now()} - Thread ID: ${Thread.currentThread().id} - Returning value")
+            70000
+        }
+    )
+
+    println("${LocalDateTime.now()} - Thread ID: ${Thread.currentThread().id} - ${future.get()}")
+}
